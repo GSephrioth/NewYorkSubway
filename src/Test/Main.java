324f89e2bbcb3f02d5eb8c9a3fafb41097af4122
GSephrioth <596620726@qq.com>
2016-11-15 11:05:14 -0600

    test file reader in Test.Main

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -3,14 +3,22 @@ package Test;
 
 import WeightedGraph.Graph;
 
-import java.io.File;
+import java.awt.image.ImagingOpException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String args[]){
-		String filePath = "DefaultGraph.txt";
-		File f = new File(filePath);
-
+        // find data files
+        String filePath = "data/agency.txt";
+        try {
+            File f = new File(filePath);
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            while(br.ready())
+                System.out.println(br.readLine());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 //        Graph graph = new Graph(f);
 //        System.out.println(graph);
 //
